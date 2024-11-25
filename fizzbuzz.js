2d232d7c9df916a87b95be12705bbc749068a41e
Tommyisready <perrot.thomas@live.fr>
2024-11-25 15:23:30 +0100

    test final CI

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,7 @@
 const fizzbuzz = (number) => {
-  if (number % 3 == 0) {
-    if (number % 5 == 0) {
-      return "FizzBuzz";
-    }
-    return "Fizz";
-  }
-  if (number % 5 === 0) {
-    return "Buzz";
-  }
+  if (number % 15 === 0) return "Fizzbuzz"; // Si divisible par 3 et 5 (3 * 5 = 15)
+  if (number % 3 === 0) return "Fizz";
+  if (number % 5 === 0) return "buzz";
   return number;
 };
 
