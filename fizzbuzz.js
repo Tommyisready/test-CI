a511051a3b8520346b8787599f78bcd9e2ac750d
Tommyisready <perrot.thomas@live.fr>
2024-11-25 15:37:33 +0100

    derniere maj

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 const fizzbuzz = (number) => {
-  if (number % 15 === 0) return "Fizzbuzz"; // Si divisible par 3 et 5 (3 * 5 = 15)
+  if (number % 15 === 0) return "Fizzbuzz";
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "buzz";
   return number;
